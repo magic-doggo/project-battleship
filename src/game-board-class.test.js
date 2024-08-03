2e6ad655acaf9c4c0dc4fbb6f91905d5365b38a1
magic-doggo <109861946+magic-doggo@users.noreply.github.com>
2024-08-03 13:10:44 +0000

    Implement throw error if try to place ship on coordinates that are taken

--- src/game-board-class.test.js
+++ src/game-board-class.test.js
@@ -6,7 +6,7 @@ import { Ship } from "./ship-class";
 test('placeShip() vertically on a board', () => {
     let ship = { length: 3 }
     let gameBoard1 = new GameBoard;
-    gameBoard1.placeShip(ship, [3, 3], "vertical");
+    gameBoard1.placeShip(ship, [3, 3], 'vertical');
     expect(gameBoard1.boardArray[33]).toBe(1);
     expect(gameBoard1.boardArray[43]).toBe(1);
     expect(gameBoard1.boardArray[53]).toBe(1);
@@ -15,7 +15,7 @@ test('placeShip() vertically on a board', () => {
 test('placeShip() horizontally on a board', () => {
     let ship = { length: 3 }
     let gameBoard1 = new GameBoard;
-    gameBoard1.placeShip(ship, [3, 3], "horizontal");
+    gameBoard1.placeShip(ship, [3, 3], 'horizontal');
     expect(gameBoard1.boardArray[33]).toBe(1);
     expect(gameBoard1.boardArray[34]).toBe(1);
     expect(gameBoard1.boardArray[35]).toBe(1);
@@ -25,22 +25,22 @@ test('placeShip() vertically to return error since length is longer than space l
     let ship = { length: 8 };
     let gameBoard1 = new GameBoard;
     expect(() => {
-        gameBoard1.placeShip(ship, [3, 3], "vertical");
+        gameBoard1.placeShip(ship, [3, 3], 'vertical');
     }).toThrow('ship does not fit vertically');
 });
 
 test('placeShip() to return an array with the boardArray coordinates of the ship', () => {
     let ship = { length: 3 };
     let gameBoard1 = new GameBoard;
-    expect(gameBoard1.placeShip(ship, [3, 3], "vertical")).toEqual([33, 43, 53]);
+    expect(gameBoard1.placeShip(ship, [3, 3], 'vertical')).toEqual([33, 43, 53]);
 });
 
 test('receiveAttack() to place X on boardArray if a ship is hit on coordinates', () => {
     let gameBoard1 = new GameBoard;
     let ship = new Ship(3);
-    gameBoard1.placeShip(ship, [3, 3], "horizontal");
+    gameBoard1.placeShip(ship, [3, 3], 'horizontal');
     gameBoard1.receiveAttack([3,3]);
-    expect(gameBoard1.boardArray[33]).toBe("x");
+    expect(gameBoard1.boardArray[33]).toBe('x');
 })
 
 test('receiveAttack() to place 0 on boardArray if attack on coordinates misses', () => {
@@ -52,7 +52,7 @@ test('receiveAttack() to place 0 on boardArray if attack on coordinates misses',
 test('placeShip() to populate arrayOfPlacedShipObjects coordinates correctly', () => {
     let gameBoard1 = new GameBoard;
     let ship = { length: 3};
-    gameBoard1.placeShip(ship, [3, 3], "horizontal");
+    gameBoard1.placeShip(ship, [3, 3], 'horizontal');
     expect(gameBoard1.arrayOfPlacedShipObjects[0].coordinates).toEqual([33,34,35])
 
 })
@@ -60,7 +60,17 @@ test('placeShip() to populate arrayOfPlacedShipObjects coordinates correctly', (
 test('receiveAttack() to increase ship nrofhitstaken', () => {
     let gameBoard1 = new GameBoard;
     let ship = new Ship(3);
-    gameBoard1.placeShip(ship, [3,3], "horizontal");
+    gameBoard1.placeShip(ship, [3,3], 'horizontal');
     gameBoard1.receiveAttack([3,3]);
     expect(ship.nrOfHitsTaken).toBe(1);
 })
+
+test('placeShip to throw error if ship would overlap with another', () => {
+    let gameBoard1 = new GameBoard;
+    let ship = new Ship(3);
+    let ship2 = new Ship(4);
+    gameBoard1.placeShip(ship, [3,3], 'horizontal');
+    expect(() => {
+      gameBoard1.placeShip(ship2, [2,4], 'vertical');
+    }).toThrow();
+  });
--- src/index.js
+++ src/index.js
@@ -4,12 +4,18 @@ import { ComputerPlayer, Player } from "./player-class";
 
 export let playerShip = new Ship(6);
 let playerShip2 = new Ship(3);
+let playerShip3 = new Ship(3)
 
 export let gameBoard1 = new GameBoard;
 
-gameBoard1.placeShip(playerShip, [3,3], "horizontal");
+console.log(gameBoard1.placeShip(playerShip, [3,3], "horizontal"));
 gameBoard1.placeShip(playerShip2, [6,2], "vertical");
 
 gameBoard1.receiveAttack([3,3]);
-console.log(playerShip.nrOfHitsTaken)
-console.log(gameBoard1.arrayOfPlacedShipObjects)
+// console.log(playerShip.nrOfHitsTaken)
+// console.log(gameBoard1.arrayOfPlacedShipObjects)
+// console.log(gameBoard1.placedShipsCoordinates)
+
+gameBoard1.placeShip(playerShip3, [2,4], "vertical");
+console.log(gameBoard1.boardArray);
+console.log("aaaaaaaaaas")
