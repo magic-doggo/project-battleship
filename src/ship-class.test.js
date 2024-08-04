055dcacb33d4757dccde505ac6d0e9ea50c90499
magic-doggo <109861946+magic-doggo@users.noreply.github.com>
2024-08-04 05:06:45 +0000

    Improve tests to not share a class instance and add test to verify ship is not sunk

--- src/ship-class.test.js
+++ src/ship-class.test.js
@@ -1,14 +1,13 @@
 import { Ship } from "./ship-class";
 
-let ship = new Ship;
-
 test('ship hit() method to increase nr of hits taken to 1', () => {
-    // let ship = new Ship(4);
+    let ship = new Ship(4);
     expect(ship.hit()).toBe(1);
 })
 
 test('ship hit() method to increase nr of hits taken to 2', () => {
-    // let ship = new Ship(4);
+    let ship = new Ship(4);
+    ship.hit();
     expect(ship.hit()).toBe(2);
 })
 
@@ -18,4 +17,12 @@ test('ship isSunk() method to change to true if nrOfHitsTaken exceeds/equals shi
         ship.hit();
     }
     expect(ship.isSunk()).toBe(true);
+})
+
+test('ship isSunk() method to change to false if nrOfHitsTaken < ship length', () => {
+    let ship = new Ship(3);
+    for (let i = 0; i < ship.length-1; i++) {
+        ship.hit();
+    }
+    expect(ship.isSunk()).toBe(false);
 })
